8ae8bf4a8e42a1ea3a00bc68c7ffda7f4b43c9a6
Inna 1201 <inna.ogurcova@gmail.com>
2022-08-03 13:18:39 +0100

    updates

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,56 @@
 var date = $('#currentDay').text(moment().format('kk:mm, dddd, MMMM Do YYYY'));
 
-var currentTime = moment().hours();
-
-var scheduleTime = $('.hour').val()
-
-console.log(scheduleTime)
-
 
 function colorBlock() {
-    for (var i = 0; i < scheduleTime.length; i++)
-        var time = scheduleTime[i]
+    var currentTime = moment().hours();
+    
+        $('.time-block').each(function () {
+           var time = parseInt($(this).attr('id'));
+           console.log(time, currentTime)
 
-    if (currentTime > time) {
-        $('.time-block').addClass('past');
-        $('.time-block').removeClass('future');
-        $('.time-block').removeClass('present');
-    } else if (currentTime === time) {
-        $('.time-block').addClass('present');
-        $('.time-block').removeClass('past');
-        $('.time-block').removeClass('future');
-    } else {
-        $('.time-block').addClass('future');
-        $('.time-block').removeClass('past');
-        $('.time-block').removeClass('present');
-    }
+           if (currentTime > time) {
+            $(this).addClass('past');
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+        } else if (currentTime === time) {
+            $(this).addClass('present');
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+        } else {
+            $(this).addClass('future');
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+        }
+    
+        });
+    
 }
-
 colorBlock()
+setInterval(colorBlock, 20000)
 
 
+
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
+$('#18 .description').val(localStorage.getItem('18'));
+
+
+
+$('.saveBtn').on('click', function () {
+  
+var taskTime = $(this).parent().attr('id');
+var taskDescription = $(this).siblings('.description').val();
+localStorage. setItem(taskTime, taskDescription);
+
+$('.hide').removeClass('hide')
+
+})
 
 
